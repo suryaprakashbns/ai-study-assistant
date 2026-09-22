1be1802ce374efab3aeff59291c4528289b28656
suryaprakashbns <167612442+suryaprakashbns@users.noreply.github.com>
2026-09-22 23:14:28 +0530

    Update llm.py

--- src/llm.py
+++ src/llm.py
@@ -20,7 +20,7 @@ def clean_response(text):
 # 1. Create the LLM
 llm = ChatGroq(
     api_key=os.getenv("GROQ_API_KEY"),
-    model="qwen/qwen3.6-27b",
+    model="qwen/qwen3.8-27b",
     temperature=0.1,
    
 )
